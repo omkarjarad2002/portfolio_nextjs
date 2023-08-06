11635f997244591f9c091ee548e3bfaa8f5a5eb5
omkar <omkarjarad2019@gmail.com>
2023-08-07 02:56:43 +0530

    first commit 07/08/2023 3am

---
+++ src/app/layout.js
@@ -0,0 +1,13 @@
+// app/layout.js
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
