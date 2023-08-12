1914ea56aeead345add36f8c9f5695ae5c1cf73d
omkar <omkarjarad2019@gmail.com>
2023-08-13 04:16:46 +0530

    13/08/2023

---
+++ src/app/Arrow.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+export default function Arrow({}: Props) {
+  return (
+    <div>
+      <div className="arrow">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faHtml5 } from "@fortawesome/free-solid-svg-icons";
+import Arrow from "./Arrow";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -25,14 +25,10 @@ export default function Home() {
             <h2 className="text-name text-2xl">PROGRAMMER</h2>
           </div>
         </div>
-        <div className="arrow">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
+        <Arrow />
         <div className="skills-container">
-          <h1 className=" skills_heading font-sans font-bold text-center text-xl scroll-m-0.5">
-            SKILLS
+          <h1 className=" skills_heading font-sans font-bold text-center text-3xl scroll-m-0.5">
+            Skills
           </h1>
           <div className="logos">
             <div className="row1">
@@ -76,8 +72,8 @@ export default function Home() {
             <div className="row2">
               <div className="html_logo ">
                 <Image
-                  className="image_html"
-                  src="/html_logo.png"
+                  className="image_logo"
+                  src="/nodejs_logo.png"
                   alt=""
                   width={350}
                   height={350}
@@ -86,7 +82,7 @@ export default function Home() {
               <div className="css_logo">
                 <Image
                   className="image_css"
-                  src="/css_logo.png"
+                  src="/mongodb_logo.png"
                   alt=""
                   width={350}
                   height={350}
@@ -95,7 +91,7 @@ export default function Home() {
               <div className="html_logo">
                 <Image
                   className="image_css"
-                  src="/js_logo.png"
+                  src="/mysql_logo.png"
                   alt=""
                   width={350}
                   height={350}
@@ -104,11 +100,98 @@ export default function Home() {
               <div className="html_logo">
                 <Image
                   className="image_css"
-                  src="/react_logo.png"
+                  src="/python_logo.png"
                   alt=""
                   width={350}
                   height={350}
                 ></Image>
+              </div>
+            </div>
+            <div className="row2">
+              <div className="html_logo nexjs_logo ">
+                <Image
+                  className="image_logo nextjs_image_logo"
+                  src="/nextjs_logo.png"
+                  alt=""
+                  width={350}
+                  height={350}
+                ></Image>
+              </div>
+              <div className="css_logo">
+                <Image
+                  className="image_css"
+                  src="/ts_logo.png"
+                  alt=""
+                  width={350}
+                  height={350}
+                ></Image>
+              </div>
+              <div className="html_logo">
+                <Image
+                  className="image_css"
+                  src="/cpp_logo.png"
+                  alt=""
+                  width={350}
+                  height={350}
+                ></Image>
+              </div>
+              <div className="html_logo">
+                <Image
+                  className="image_css"
+                  src="/dart_logo.png"
+                  alt=""
+                  width={350}
+                  height={350}
+                ></Image>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="temp">
+          <h1 className=" skills_heading font-sans font-bold text-center text-3xl scroll-m-0.5">
+            Problem solving
+          </h1>
+          <Arrow />
+          <div className="leetcode_div">
+            <Link href="https://leetcode.com/jaradomkar24/">
+              <div className="leetcode_logo ">
+                <Image
+                  className="image_leetcode"
+                  src="/leetcode_logo.svg"
+                  alt=""
+                  width={350}
+                  height={350}
+                ></Image>
+              </div>
+            </Link>
+          </div>
+        </div>
+        <div className="projects_div">
+          <h1 className=" skills_heading font-sans font-bold text-center text-3xl scroll-m-0.5">
+            Projects
+          </h1>
+          <Arrow />
+          <div className="projects">
+            <div className="row1">
+              <div className="project">
+                <div className="project_image">
+                  <Image
+                    className="blog_image"
+                    src="/blog_image.png"
+                    alt=""
+                    width={350}
+                    height={350}
+                  ></Image>
+                </div>
+                <div className="project_decs">
+                  <h1 className="text-5xl tracking-wide my-4">OMKAR</h1>
+                  <p className="text-xl my-10">
+                    This project is related to blogs. Blog is much more
+                    important. Blogs provides much more information.Blogs are
+                    the most important part of every people as well as
+                    buissness.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
